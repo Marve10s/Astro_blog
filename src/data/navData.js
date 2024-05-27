2a5bfde0893caf25e4eeeb215e47c49379fdc60c
Marve10s <igrimanigroman@gmail.com>
2024-05-27 17:43:50 +0300

    Added new pages,posts

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+  {
+    name: "About",
+    path: "/about/",
+  },
+  {
+    name: "Blog",
+    path: "/blog/",
+  },
+  {
+    name: "Portfolio",
+    path: "https://my-potfolio-web.netlify.app",
+  },
+];
+
+export default navData;
