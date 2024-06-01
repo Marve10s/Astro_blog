f58b749ebcba6cd2b65a4f3268812d9504bf5cbb
Marve10s <igrimanigroman@gmail.com>
2024-06-02 01:16:53 +0300

    Bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "",
+  site: "https://astro-sitemap.netlify.app",
   integrations: [icon(), sitemap()],
 });
