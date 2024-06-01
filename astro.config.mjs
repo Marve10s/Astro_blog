ca5467712239b6eff21c2a9f4557b49eac0b03e3
Marve10s <igrimanigroman@gmail.com>
2024-06-02 01:15:55 +0300

    Test bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), sitemap()]
+  site: "",
+  integrations: [icon(), sitemap()],
 });
